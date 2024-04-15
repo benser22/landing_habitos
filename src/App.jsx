33e9fd57017682d3da995827827817bba24a9c6e
Benjamín Serrano Friedlander <benser22@hotmail.com>
2024-04-15 16:55:56 -0300

    docs(json): json file was created with the corresponding information from the landing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import React from "react";
+import data from "./data.json";
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  console.log(data.sections);
+  return (
+    <div>
+      <div>
+        <h2>{data.sections.header.title}</h2>
+        <p>{data.sections.header.description}</p>
+        <img src={data.sections.header.logo} alt={data.sections.header.alt} />
+        {data.sections.header.text_logo}
+      </div>
+
+      <div>
+        <h2>{data.sections.one.title}</h2>
+        <img src={data.sections.one.image} alt={data.sections.one.alt} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
