3ae34e469c42216bf8f806f29874883d896b9302
Benjamín Serrano Friedlander <benser22@hotmail.com>
2024-04-16 18:05:44 -0300

    feat(fourSection): the four section component was created

--- src/components/Sections/FourSection.jsx
+++ src/components/Sections/FourSection.jsx
@@ -1,7 +1,42 @@
 import React from "react";
 import data from "../../data.json";
+import bg from "../../assets/section-six-bg.png";
 
 function FourSection() {
-  return <div className="flex justify-center p-8 bg-gray-500">Sección 4</div>;
+  return (
+    <div
+      className="w-full h-full p-8 py-32"
+      style={{
+        backgroundImage: `url(${bg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="sm:grid sm:grid-cols-2 gap-6">
+        <p className="text-white col-span-2 text-2xl w-full lg:w-1/2 py-6">
+          {data.sections.six.title}
+        </p>
+        <div className="flex items-center sm:items-end gap-6 sm:gap-3 py-6 justify-center">
+          <p className="text-white text-7xl">01.</p>
+          <div className="flex flex-col gap-4">
+            <p className="text-white text-xl">{data.sections.six.subtitle1}</p>
+            <p className="text-white text-md">
+              {data.sections.six.description}
+            </p>
+          </div>
+        </div>
+        <div className="flex items-center sm:items-end gap-3 py-6 justify-center">
+          <p className="text-white text-7xl">02.</p>
+          <div className="flex flex-col gap-4">
+            <p className="text-white text-xl">{data.sections.six.subtitle2}</p>
+            <p className="text-white text-md">
+              {data.sections.six.description}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
+
 export default FourSection;
--- src/components/utils/circles.jsx
+++ src/components/utils/circles.jsx
@@ -1,28 +1,43 @@
 import React from "react";
 
-function Circles({ largestDiameter, color }) {
-  const diameter1 = largestDiameter;
-  const diameter2 = largestDiameter - 20;
-  const diameter3 = largestDiameter - 40;
+function Circles({ diameter, x, y }) {
+  const diameter1 = diameter;
+  const diameter2 = diameter / 1.3;
+  const diameter3 = diameter / 2.2;
 
-  console.log(diameter1, diameter2, diameter3);
   return (
-    <div className="flex justify-center items-center h-[500px] bg-yellow-400 relative">
+    <div className="relative opacity-30">
       <div
-        className={`w-[${diameter1}px] h-[${diameter1}px] bg-primar rounded-full flex items-center justify-center`}
+        style={{ left: `${x}px`, top: `${y}px` }}
+        className="absolute transform -translate-x-1/2 -translate-y-1/2"
       >
-        {diameter1}
-      </div>
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <div
-          className={`w-${diameter2} h-${diameter2} bg-${color}-200 rounded-full flex items-center justify-center`}
+          style={{ width: `${diameter1}px`, height: `${diameter1}px` }}
+          className="bg-white bg-opacity-50 rounded-full flex items-center justify-center"
         >
-          {diameter2}
+          {}
         </div>
+      </div>
+      <div
+        style={{ left: `${x}px`, top: `${y}px` }}
+        className="absolute transform -translate-x-1/2 -translate-y-1/2"
+      >
         <div
-          className={`w-${diameter3} h-${diameter3} bg-${color}-400 rounded-full flex items-center justify-center`}
+          style={{ width: `${diameter2}px`, height: `${diameter2}px` }}
+          className="bg-white bg-opacity-60 rounded-full flex items-center justify-center"
         >
-          {diameter3}
+          {}
+        </div>
+      </div>
+      <div
+        style={{ left: `${x}px`, top: `${y}px` }}
+        className="absolute transform -translate-x-1/2 -translate-y-1/2"
+      >
+        <div
+          style={{ width: `${diameter3}px`, height: `${diameter3}px` }}
+          className="bg-white bg-opacity-95 rounded-full flex items-center justify-center"
+        >
+          {}
         </div>
       </div>
     </div>
