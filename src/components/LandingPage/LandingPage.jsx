d94576fd5d0b09244d291ae06d0a2c10e016341d
Benjamín Serrano Friedlander <benser22@hotmail.com>
2024-04-17 11:37:31 -0300

    feat(loading): loading added to landing

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { HiOutlineRefresh } from "react-icons/hi";
 import Navbar from "../Navbar/Navbar";
 import Hero from "../Hero/Hero";
 import Layout from "../Layout/Layout";
 import Body from "../Body/Body";
 import Footer from "../Footer/Footer";
+
+function LoadingSpinner() {
+  return (
+    <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-gray-800 bg-opacity-75 z-50">
+      <div className="text-white">
+        <HiOutlineRefresh className="animate-spin h-8 w-8 mr-2" />
+        Loading...
+      </div>
+    </div>
+  );
+}
+
+const LazyLandingPage = React.lazy(() => import("./LandingPage"));
+
+function App() {
+  return (
+    <Suspense fallback={<LoadingSpinner />}>
+      <LazyLandingPage />
+    </Suspense>
+  );
+}
 
 function LandingPage() {
   return (
