30f10c1e2ae947a1dd21db76c2d7adebf74e557e
Benjamín Serrano Friedlander <benser22@hotmail.com>
2024-05-09 17:29:32 -0300

    Changes(App): changes to Marketing

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,67 +1,41 @@
-import React from "react";
-import data from "../../data.json";
+import React from 'react';
+import data from '../../data.json';
 
 function Hero() {
-  const words = data["section-one"].title.split(" ");
+  const words = data['section-one'].title.split(' ');
 
   return (
     <div className="relative">
       <div className="grid sm:grid-cols-2 gap-2 mt-16 sm:mt-20">
-        <div className="flex flex-col justify-center items-start text-3xl sm:text-4xl md:text-6xl lg:text-7xl font-bold">
-          <h2 style={{ lineHeight: "1.2" }}>
+        <div className="px-4 flex flex-col justify-center items-center text-3xl sm:text-4xl md:text-5xl font-bold">
+          <h2
+            className="text-left sm:text-end mb-4"
+            style={{ lineHeight: '1.2' }}
+          >
             {words.map((word, index) => (
               <span
                 key={index}
                 style={{
-                  color: word === "expertise" ? "#2B88D9" : "inherit",
+                  color: word === 'Connecting' ? '#2B88D9' : 'inherit',
                 }}
               >
-                {word}{" "}
+                {word}{' '}
               </span>
             ))}
           </h2>
-          <h2 className="text-[#F2F2F2] sm:mb-60">\\ Emergency</h2>
           <img
-            src={data["section-one"].image}
-            alt={data["section-one"].alt}
-            width="100%"
+            src={data['section-one'].image}
+            alt={data['section-one'].alt}
             height="auto"
-            className="block sm:hidden"
+            className="block sm:hidden mx-auto w-full rounded-md mb-4"
           />
         </div>
         <div className="hidden sm:block">
-          <img src={data["section-one"].image} alt={data["section-one"].alt} />
-        </div>
-      </div>
-      <div
-        className="sm:absolute backdrop-blur-[0.5px] top-0 sm:top-[50%] md:top-[70%] left-0 bg-white bg-opacity-85 w-full h-auto py-4 z-10"
-        style={{
-          boxShadow: "0px 0px 1px 0px rgba(0, 0, 0, 0.1)",
-        }}
-      >
-        <div className="py-4 px-12 grid">
-          <p className="text-2xl font-[500] mb-3 tracking-normal">
-            {data["section-two"].title}
-          </p>
-          <div className="flex flex-col sm:flex-row border-s-[#DDDDDD]">
-            {Object.entries(data["section-two"].filtersbar).map(
-              ([filterTitle, options]) => (
-                <select
-                  key={filterTitle}
-                  className={`border border-gray_light text-gray_default px-6 py-3.5 w-full sm:w-[18%] cursor-pointer`}
-                >
-                  {options.map((option, index) => (
-                    <option key={index} value={option}>
-                      {option}
-                    </option>
-                  ))}
-                </select>
-              )
-            )}
-            <button className="bg-primary sm:ml-4 mt-2 sm:mt-0 w-full sm:w-1/5 py-3 text-white">
-              {data["section-two"].button}
-            </button>
-          </div>
+          <img
+            src={data['section-one'].image}
+            alt={data['section-one'].alt}
+            className="mx-auto w-full rounded-md"
+          />
         </div>
       </div>
     </div>
--- src/components/Sections/FourSection.jsx
+++ src/components/Sections/FourSection.jsx
@@ -1,39 +1,39 @@
-import React from "react";
-import data from "../../data.json";
+import React from 'react';
+import data from '../../data.json';
 
 function FourSection() {
   return (
     <div
-      className="w-full h-full p-8 py-20 sm:py-32"
+      className="w-full h-full p-8 py-10 sm:py-12"
       style={{
         backgroundImage: `url(${data.backgrounds[1]})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
       }}
     >
-      <div className="sm:grid sm:grid-cols-2 gap-6">
-        <p className="text-white col-span-2 text-2xl w-full lg:w-1/2 py-6">
-          {data["section-six"].title}
+      <div className="sm:grid sm:grid-cols-2 gap-6 items-start">
+        <p className="text-white col-span-2 text-2xl w-full lg:w-1/2 py-6 font-bold">
+          {data['section-six'].title}
         </p>
         <div className="flex items-center sm:items-end gap-6 sm:gap-3 py-6 justify-center">
           <p className="text-white text-7xl">01.</p>
           <div className="flex flex-col gap-4">
-            <p className="text-white text-xl">
-              {data["section-six"].subtitle1}
+            <p className="text-white text-xl font-semibold">
+              {data['section-six'].subtitle1}
             </p>
             <p className="text-white text-md">
-              {data["section-six"].description}
+              {data['section-six'].description1}
             </p>
           </div>
         </div>
         <div className="flex items-center sm:items-end gap-3 py-6 justify-center">
           <p className="text-white text-7xl">02.</p>
           <div className="flex flex-col gap-4">
-            <p className="text-white text-xl">
-              {data["section-six"].subtitle2}
+            <p className="text-white text-xl font-semibold">
+              {data['section-six'].subtitle2}
             </p>
-            <p className="text-white text-md">
-              {data["section-six"].description}
+            <p className="text-white text-md w-5/6">
+              {data['section-six'].description2}
             </p>
           </div>
         </div>
